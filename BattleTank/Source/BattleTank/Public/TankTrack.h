1be944cbf8301e665e0419aa942b9e7e275fca3d
Adolfo-PC\Adolfo <adolfo.sobrino@gmail.com>
2017-10-16 18:22:10 +0200

    155. ApplyForceAtLocation() in Action

--- BattleTank/Source/BattleTank/Public/TankTrack.h
+++ BattleTank/Source/BattleTank/Public/TankTrack.h
@@ -18,5 +18,7 @@ public:
 	UFUNCTION(BlueprintCallable,Category=Input)
 	void SetThrottle(float Throttle);
 	
-	
+	//Max Force per Track in Newtons
+	UPROPERTY(EditDefaultsOnly)
+		float TrackMaxDrivingForce = 400000;//Assume 40 tonne tank, and 1g acceleration.
 };
